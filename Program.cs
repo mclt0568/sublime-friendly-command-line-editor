806c39f4d2a9824e997fbf58a6bf715b09268e75
Frankium <tis7bfrankie@gmail.com>
2021-07-07 07:28:48 +0800

    initialized project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sublime_friendly_command_line_editor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
